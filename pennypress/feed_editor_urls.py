3746415feaf2413d9c91bf2f7f0078a5b383c4d6
Glenn Franxman <glenn.franxman@scripps.com>
2015-03-22 02:48:29 -0400

    editing individual feeds. still need to delete individual items from a feed.

--- pennypress/feed_editor_urls.py
+++ pennypress/feed_editor_urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^static/(?P<path>.*)', 'pennypress.feed_editor_views.bootstrap', name='feed_editor_static'),
-    url(r'^reset_sortkey_for_feeditem/(?P<feeditem_id>.*)/', 'pennypress.feed_editor_views.reset_sortkey_for_feeditem', name='reset_sortkey_for_feeditem'),
-    url(r'^$', 'pennypress.feed_editor_views.feed_editor', name='feed_editor'),
+    url(r'^(?P<feed_slug>.*)/reset_sortkey_for_feeditem/(?P<feeditem_id>.*)/', 'pennypress.feed_editor_views.reset_sortkey_for_feeditem', name='reset_sortkey_for_feeditem'),
+    url(r'^(?P<feed_slug>.*)/$', 'pennypress.feed_editor_views.feed_editor', name='feed_editor'),
+    url(r'^$', 'pennypress.feed_editor_views.feed_editor', {'feed_slug':'local'}, name='feed_editor_home'),
 )
--- pennypress/feed_editor_views.py
+++ pennypress/feed_editor_views.py
@@ -4,15 +4,19 @@ from django.template import RequestContext
 
 from pennypress.models import Stream, Item, Feed, FeedItem
 
+from decimal import Decimal, getcontext
+from sets import Set
+
 def feed_editor(request, feed_slug="local"):
     t = loader.get_template( 'feed_editor/index.html' )
 
     f = Feed.objects.get(slug=feed_slug)
 
     c = RequestContext( request, {
-        'streams':Stream.objects.all(),
-        'stream_items': Item.objects.all(),
+        'streams':f.streams.all(),
+        'stream_items':  reduce( Set.union, map( Set, [ x.item_set.all() for x in f.streams.all()])), #Item.objects.all(),
         'feed': f,
+        'all_feeds': Feed.objects.all(),
     } )
     return HttpResponse( t.render( c ) )
 
@@ -21,11 +25,15 @@ def bootstrap( request, path ):
     c = RequestContext( request, {} )
     return HttpResponse( t.render( c ) )
 
-def reset_sortkey_for_feeditem( request, feeditem_id ):
-    from decimal import *
-    getcontext().prec = 28
-    feeditem = FeedItem.objects.get( id = feeditem_id )
-    feeditem.sort_key = Decimal( request.REQUEST.get('sort-key') )
-    feeditem.save()
-    return HttpResponse( "Thanks" )
+def reset_sortkey_for_feeditem( request, feed_slug, feeditem_id ):
+    try:
+        getcontext().prec = 28
+        feeditem = FeedItem.objects.get( id = feeditem_id )
+        feeditem.sort_key = Decimal( request.REQUEST.get('sort-key') )
+        feeditem.save()
+        return HttpResponse( "Thanks" )
+    except Exception, e:
+        print "oops", e
+    return HttpResponse( "oops" )
+
 
