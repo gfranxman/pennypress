1e9ca0668e42db4e27cc89648f567efb8a6b3aba
Glenn Franxman <glenn.franxman@scripps.com>
2015-03-22 00:19:17 -0400

    FeedItems in the Feed Editor

--- pennypress/feed_editor_views.py
+++ pennypress/feed_editor_views.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse
 from django.template import loader
 from django.template import RequestContext
 
-from pennypress.models import Stream, Item
-def feed_editor(request):
+from pennypress.models import Stream, Item, Feed
+
+def feed_editor(request, feed_slug="local"):
     t = loader.get_template( 'feed_editor/index.html' )
+
+    f = Feed.objects.get(slug=feed_slug)
+
     c = RequestContext( request, {
         'streams':Stream.objects.all(),
         'stream_items': Item.objects.all(),
+        'feed': f,
     } )
     return HttpResponse( t.render( c ) )
 
